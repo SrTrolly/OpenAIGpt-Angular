0e7c842613cf3161c9cbd7da3e149fc9a56b5d18
Nicolas Inostroza <ninostrozacordova2016@gmail.com>
2024-06-09 20:11:41 -0400

    migrar a angular 18

---
+++ src/app/presentation/components/text-boxes/textMessagesBox/textMessagesBox.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Inject, Input, Output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-messages-box',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './textMessagesBox.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TextMessagesBoxComponent {
+  private fb: FormBuilder = Inject(FormBuilder);
+
+  @Input() public placeholder: string = "";
+  @Input() public disableCorrections: boolean = false;
+
+  @Output() public onMessage: EventEmitter<string> = new EventEmitter<string>();
+
+  public form = this.fb.group({
+    prompt: ["", Validators.required],
+  });
+  // constructor(private fb: FormBuilder) { }
+
+
+  handleSubmit() {
+    if (this.form.invalid) return;
+    const { prompt } = this.form.value;
+    console.log({ prompt });
+    this.onMessage.emit(prompt ?? "");
+    this.form.reset();
+  }
+
+}
--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChatMessageComponent, MyMessageComponent, TextMessagesBoxComponent, TypingLoaderComponent } from '@components/index';
+
+
 
 @Component({
   selector: 'app-orthography-page',
   standalone: true,
   imports: [
     CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessagesBoxComponent
   ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OrthographyPageComponent { }
+export default class OrthographyPageComponent { }
